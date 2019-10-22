664d22a3141b9beebf6ef123394d943f3a4d1154
JuanRubiio <juan12_rubio@hotmail.com>
2019-10-23 00:20:38 +0200

    modificaciones

--- src/main/java/acme/features/anonymous/jimenezbulletin/AnonymousJimenezBulletinRepository.java
+++ src/main/java/acme/features/anonymous/jimenezbulletin/AnonymousJimenezBulletinRepository.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import acme.entities.bulletin.JimenezBulletin;
+import acme.entities.bulletins.JimenezBulletin;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
